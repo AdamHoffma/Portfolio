cbcb4a8475a81737f9b0ec6567e7df3c49193616
Adam Hoffman <wintros@yahoo.com>
2020-05-07 16:44:29 -0600

    style icon

--- client/src/Components/Title/Title.js
+++ client/src/Components/Title/Title.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import image from '../Resources/Beautiful_Sunset_For_Portfolio.jpeg'
+import arrow from '../Resources/arrow.png'
 import './Title.css'
 
 const Title = () => {
@@ -7,6 +8,7 @@ const Title = () => {
     return (
         <div className="overlay-image">            
             <img className="title-image" src={image} alt="Image of sunset"></img>
+            <img className="arrow" src={arrow} alt="arrow to about"/>
             <div className="text">"The only thing I love more then building a beautiful webpage is a beautiful Colorado Sunset" -Adam Hoffman</div>
         </div>
     )
